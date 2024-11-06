b5bd5587f18898435885e0eec97c66bed2c743cf
arpit <arpit2003y@gmail.com>
2024-11-06 17:03:06 +0530

    fix(profile):fixed_profile_loading

--- src/components/Select.tsx
+++ src/components/Select.tsx
@@ -1,34 +1,34 @@
 import React, { useId } from "react"
 
 interface SelectProps extends React.SelectHTMLAttributes<HTMLSelectElement> {
-  options: string[]
-  label?: string
-  className?: string
+     options: string[]
+     label?: string
+     className?: string
 }
 
 const Select: React.ForwardRefRenderFunction<HTMLSelectElement, SelectProps> = (
-  { options, label, className = "", ...props }: SelectProps,
-  ref
+     { options, label, className = "", ...props }: SelectProps,
+     ref
 ) => {
-  const id = useId()
-  return (
-    <div className="w-full text-black">
-      {label && <label htmlFor={id}>{label}</label>}
-      <select
-        {...props}
-        name=""
-        id={id}
-        className={`px-3 py-2 rounded-lg bg-white text-black outline-none focus:bg-gray-50 duration-200 border border-gray-200 w-full ${className}`}
-        ref={ref}
-      >
-        {options?.map((current, index) => (
-          <option value={current} key={index}>
-            {current}
-          </option>
-        ))}
-      </select>
-    </div>
-  )
+     const id = useId()
+     return (
+          <div className="w-full text-black">
+               {label && <label htmlFor={id}>{label}</label>}
+               <select
+                    {...props}
+                    name=""
+                    id={id}
+                    className={`px-3 py-2 rounded-lg bg-white text-black outline-none focus:bg-gray-50 duration-200 border border-gray-200 w-full ${className}`}
+                    ref={ref}
+               >
+                    {options?.map((current, index) => (
+                         <option value={current} key={index}>
+                              {current}
+                         </option>
+                    ))}
+               </select>
+          </div>
+     )
 }
 
 export default React.forwardRef(Select)
--- src/pages/profile.tsx
+++ src/pages/profile.tsx
@@ -11,57 +11,63 @@ import { bookmarks as bookmarksService } from "../services/bookmarks.ts"
 const ProfileComponent = lazy(() => import("../components/Profile/profile.tsx"))
 
 export default function ProfilePage(): JSX.Element {
-	const { userId } = useParams<{ userId: string }>()
-	const { profile, isAuthor, loading, setLoading } = useProfile(userId as string)
-	const [bookmarks, setBookmarks] = useState<
-		{ articleId: string; post: { title: string }; createdAt: string }[]
-	>([])
-	const navigate = useNavigate()
+     const { userId } = useParams<{ userId: string }>()
+     const { profile, isAuthor, loading } = useProfile(userId as string)
+     const [bookmarks, setBookmarks] = useState<
+          { articleId: string; post: { title: string }; createdAt: string }[]
+     >([])
+     const [bookmarksLoading, setBookmarksLoading] = useState(false)
+     const navigate = useNavigate()
 
-	useEffect(() => {
-		; (async () => {
-			setLoading(true)
-			if (userId) {
-				const response =
-					await bookmarksService.getUserBookmarks(userId as string)
-				if (response) setBookmarks(response)
-			}
-			setLoading(false)
-		})()
-	}, [profile, userId])
+     useEffect(() => {
+          ;(async () => {
+               setBookmarksLoading(true)
+               if (userId) {
+                    const response = await bookmarksService.getUserBookmarks(
+                         userId as string
+                    )
+                    if (response) setBookmarks(response)
+               }
+               setBookmarksLoading(false)
+          })()
+     }, [userId])
 
-	if (!loading && (!userId || !profile || userId === "undefined"))
-		return <Fallback />
+     if (
+          !loading &&
+          !bookmarksLoading &&
+          (!userId || !profile || userId === "undefined")
+     )
+          return <Fallback />
 
-	return (
-		<div className="py-8">
-			<Suspense fallback={profileFallbackLoader()}>
-				<div className="back w-1/4">
-					<Button
-						className="md:w-1/3 w-1/2 flex  items-center justify-center bg-transparent font-bold gap-2"
-						onClick={() => navigate("/")}
-					>
-						<MoveLeft size={15} fontWeight={800} />
-						Back
-					</Button>
-				</div>
-				<ProfileComponent
-					data={profile as IUserProfile}
-					isAuthor={isAuthor}
-					loading={loading}
-					bookmarks={bookmarks}
-				/>
-			</Suspense>
-		</div>
-	)
+     return (
+          <div className="py-8">
+               <Suspense fallback={profileFallbackLoader()}>
+                    <div className="back w-1/4">
+                         <Button
+                              className="md:w-1/3 w-1/2 flex  items-center justify-center bg-transparent font-bold gap-2"
+                              onClick={() => navigate("/")}
+                         >
+                              <MoveLeft size={15} fontWeight={800} />
+                              Back
+                         </Button>
+                    </div>
+                    {!loading && !bookmarksLoading && (
+                         <ProfileComponent
+                              data={profile as IUserProfile}
+                              isAuthor={isAuthor}
+                              loading={loading}
+                              bookmarks={bookmarks}
+                         />
+                    )}
+               </Suspense>
+          </div>
+     )
 }
 
-
-
 const profileFallbackLoader = () => {
-	return (
-		<div className="py-24 flex items-center justify-center">
-			<Loader2 className="mr-2 h-10 w-10 animate-spin" />
-		</div>
-	)
+     return (
+          <div className="py-24 flex items-center justify-center">
+               <Loader2 className="mr-2 h-10 w-10 animate-spin" />
+          </div>
+     )
 }
